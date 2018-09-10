488cc470da7196f43f02da2133dd4d9d66128877
abradle <you@example.com>
2018-09-10 16:48:45 +0100

    Raw debug

--- loader.py
+++ loader.py
@@ -20,5 +20,6 @@ if __name__ == "__main__":
 
     prefix = "/code/media/NEW_DATA/"
     targets_to_load = get_target_list(prefix)
+    print("THIS POD HAS UPDATED")
     for target_name in targets_to_load:
         process_target(prefix, target_name)
