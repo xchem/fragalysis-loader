7a27cd38db56cbbabb8b1a5c42e125c4cdeb718f
Alan Christie <alan.christie@matildapeak.com>
2020-09-18 15:30:53 +0100

    - Another attempt at a loader fix

--- specialised-loaders/s3-loader/docker-entrypoint.sh
+++ specialised-loaders/s3-loader/docker-entrypoint.sh
@@ -39,23 +39,26 @@ mkdir ${DST}
 # List the bucket's objects (files).
 # Output is typically: -
 #
-#   2020-09-16 12:56:32      12288 django-data/ALL_TARGETS/.TARGET_LIST.swp
-#   2020-09-16 12:56:32       1886 django-data/ALL_TARGETS/ALL_TARGETS/ATAD/ATAD2A-x1712_1/ATAD2A-x1712_1.mol
-#   2020-09-16 12:56:32       4064 django-data/ALL_TARGETS/ALL_TARGETS/ATAD/ATAD2A-x1712_1/ATAD2A-x1712_1.mol2
+#   2020-09-16 12:56:32      12288 django-data/2020-09-15T16/READY
+#   2020-09-16 12:56:32       1886 django-data/2020-09-15T16/TARGET_LIST
+#   2020-09-16 12:56:32       4064 django-data/2020-09-15T16/ATAD/ATAD2A-x1712_1/ATAD2A-x1712_1.mol2
 #
 # And we want...
 #
-#   ALL_TARGETS/.TARGET_LIST.swp
-#   ALL_TARGETS/ALL_TARGETS/ATAD/ATAD2A-x1712_1/ATAD2A-x1712_1.mol
-#   ALL_TARGETS/ALL_TARGETS/ATAD/ATAD2A-x1712_1/ATAD2A-x1712_1.mol2
+#   django-data/2020-09-15T16/READY
+#   django-data/2020-09-15T16/TARGET_LIST
+#   django-data/2020-09-15T16/ATAD/ATAD2A-x1712_1/ATAD2A-x1712_1.mol2
 BUCKET_PATH="${BUCKET_NAME}/django-data/${DATA_ORIGIN}"
 echo "+> Listing S3 path (${BUCKET_PATH})..."
-PATH_OBJECTS=$(aws s3 ls --recursive "s3://${BUCKET_PATH}" | tr -s ' ' | cut -d ' ' -f 4 | cut -c13-)
+PATH_OBJECTS=$(aws s3 ls --recursive "s3://${BUCKET_PATH}" | tr -s ' ' | cut -d ' ' -f 4)
 
 # Now copy each object to the media's NEW_DATA directory
+# The PATH_OBJECT 'django-data/2020-09-15T16/TARGET_LIST'
+# is written as 'TARGET_LIST' to '/code/media/NEW_DATA'
 echo "+> Copying objects..."
-for PATH_OBJECT in $PATH_OBJECTS; do
-  aws s3 cp "s3://${BUCKET_NAME}/django-data/${PATH_OBJECT}" "/code/media/NEW_DATA/${PATH_OBJECT}"
+for SRC_OBJECT in $PATH_OBJECTS; do
+  DST_OBJECT=$(echo "${SRC_OBJECT}" | cut -f 3- -d '/');
+  aws s3 cp "s3://${BUCKET_NAME}/${SRC_OBJECT}" "/code/media/NEW_DATA/${DST_OBJECT}";
 done
 echo "+> Copied."
 
